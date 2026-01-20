738def96c74a63a50149e653587318978c8a2264
Roj <rojvv@icloud.com>
2026-01-20 21:49:25 +0300

    Fix build

--- src/stages/SignedIn.tsx
+++ src/stages/SignedIn.tsx
@@ -1,5 +1,5 @@
 import { createSignal } from "solid-js";
-import { setStage, type Stage, stage } from "../state/stage";
+import { type Stage, stage } from "../state/stage";
 import "./SignedIn.css";
 import { client } from "../actions/initClient";
 
